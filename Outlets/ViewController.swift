facedb2b88c870bfe428cf5732be6720e0f56b8d
Alumno <ulsa@d109.local>
2022-08-16 09:07:17 -0700

    Initial Commit

---
+++ Outlets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Outlets
+//
+//  Created by Alumno on 16/08/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
